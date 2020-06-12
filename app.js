670977d8d53aaa85840a8a5c6ff41c575d8db228
PRATHAM PONNANA <46704977+prathamponnana@users.noreply.github.com>
2020-06-12 17:28:46 +0530

    Add files via upload
    >New picture with caption and heading
    >Added local storage feature

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-let count = 0;
-document.querySelector('.my-pic').addEventListener('click', function(){
-    count++;
-    document.getElementById('btn').innerHTML= count;
-    localStorage.setItem('count','count');
+document.addEventListener('DOMContentLoaded', getCount);
+let counter1 = 0;
+let counter2 = 0;
+document.querySelector('.my-pic1').addEventListener('click', function(){ 
+    counter1++;
+    localStorage.setItem('counter1',counter1);
+    document.getElementById('btn1').innerHTML= localStorage.getItem('counter1');
 });
+
+document.querySelector('.my-pic2').addEventListener('click', function(){
+    counter2++;
+    localStorage.setItem('counter2',counter2);
+    document.getElementById('btn2').innerHTML= localStorage.getItem('counter2');
+});
+
+function getCount(){
+    document.getElementById('btn1').innerHTML= localStorage.getItem('counter1');
+    document.getElementById('btn2').innerHTML= localStorage.getItem('counter2');
+    counter1 = localStorage.getItem('counter1');
+    counter2 = localStorage.getItem('counter2');
+}
