4a2d7015eea6b2b7e0c0228b432b04d3562e0f8e
PRATHAM PONNANA <46704977+prathamponnana@users.noreply.github.com>
2020-06-18 21:57:57 +0530

    Update app.js
    Optimized the logic.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', getCount);
-let counter1 = 0;
-let counter2 = 0;
-document.querySelector('.my-pic1').addEventListener('click', function(){ 
-    counter1++;
-    localStorage.setItem('counter1',counter1);
-    document.getElementById('btn1').innerHTML= localStorage.getItem('counter1');
+document.querySelector('.my-pic1').addEventListener('click', function(){Counter('btn1');});
+
+document.querySelector('.my-pic2').addEventListener('click', function(){Counter('btn2');});
+
+document.addEventListener('DOMContentLoaded', function(){
+      getCount();
 });
 
-document.querySelector('.my-pic2').addEventListener('click', function(){
-    counter2++;
-    localStorage.setItem('counter2',counter2);
-    document.getElementById('btn2').innerHTML= localStorage.getItem('counter2');
-});
+function Counter(btn){
+    const currElem = document.getElementById(btn);
+    const currCount = parseInt(currElem.innerText);
+    localStorage.setItem(btn,currCount+1);
+    currElem.innerText= localStorage.getItem(btn);   
+}
 
 function getCount(){
-    document.getElementById('btn1').innerHTML= localStorage.getItem('counter1');
-    document.getElementById('btn2').innerHTML= localStorage.getItem('counter2');
-    counter1 = localStorage.getItem('counter1');
-    counter2 = localStorage.getItem('counter2');
+    const storageVal1 = localStorage.getItem(btn);
+    const value1 = !storageVal1 && isNan(storageVal1)?0:localStorage.getItem(btn);
+    document.getElementById(btn).innerText = value1;
 }
+    
