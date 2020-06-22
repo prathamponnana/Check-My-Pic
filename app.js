c435cf77629161ae9f37e034e06d41e6f834ef55
PRATHAM PONNANA <46704977+prathamponnana@users.noreply.github.com>
2020-06-22 22:08:53 +0530

    Update app.js
    Improved optimization

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
+import {lsGetItem, storageChecker} from './util.js';
+
 document.querySelector('.my-pic1').addEventListener('click', function(){Counter('btn1');});
 
 document.querySelector('.my-pic2').addEventListener('click', function(){Counter('btn2');});
 
 document.addEventListener('DOMContentLoaded', function(){
-      getCount();
+      getCount(); 
 });
 
 function Counter(btn){
-    const currElem = document.getElementById(btn);
-    const currCount = parseInt(currElem.innerText);
-    localStorage.setItem(btn,currCount+1);
-    currElem.innerText= localStorage.getItem(btn);   
+      const currElem = document.getElementById(btn);
+      const currCount = parseInt(currElem.innerText);
+      localStorage.setItem(btn,currCount+1);
+      currElem.innerText= localStorage.getItem(btn);  
 }
 
 function getCount(){
-    const storageVal1 = lsGetItem('btn1');
-    const value1 = !storageVal1 && isNaN(storageVal1)?0:lsGetItem('btn1');
-    document.getElementById('btn1').innerText = value1;
-
-    const storageVal2 = lsGetItem('btn2');
-    const value2 = !storageVal2 && isNaN(storageVal2)?0:lsGetItem('btn2');
-    document.getElementById('btn2').innerText = value2;
-}
-
-function lsGetItem(key){
-    return localStorage.getItem(key);
+    let buttons = Array.from(document.getElementsByClassName('btn'));
+      for(var i =0; i<localStorage.length; i++){
+            for(var j=0; j<buttons.length;j++){
+                var value = storageChecker(i); 
+            }
+            buttons[i].innerText = value;
+      }
 }
